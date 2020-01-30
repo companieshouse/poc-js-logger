845525caf6a2c5b52877bba39c3af8bec583377d
csciubbach <csciubba@companieshouse.gov.uk>
2020-01-30 17:09:55 +0000

    Linted files

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 PoC for logging in JavaScript for Node.JS applications
 
 
-Env variables:
+## Env variables:
 LOG_LEVEL
 HUMAN_LOG
+
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const createLogger = require("./lib/createLogger");
 const chLogger = function (options) {
 
     const logger = createLogger(options);
+
     logger.middleware = createMiddleware(logger);
 
     return logger;
